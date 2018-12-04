a017f127ee59198e95bd2d76b245e120ad8cf13f
Rachel Slagle <rasl3673@colorado.edu>
2018-12-04 09:28:44 -0700

    Uploading Project 2 Prototype

---
+++ projects/project2/Prototype/app/src/main/java/com/example/raychill/prototype/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.raychill.prototype;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
